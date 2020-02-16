e1330bda129a2e9e93b0678af261f6dd7c5db951
Ian Applekamp <ieapplekamp@gmail.com>
2020-02-16 18:04:40 -0500

    the clouds finally move across the screen, but having a lot of issues converting keyframes to mixins

--- portfolio/src/components/Header/Intro.js
+++ portfolio/src/components/Header/Intro.js
@@ -1,47 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import "../Header/Intro.css"
 
-function Intro() {
-    return (
-		<div className="container">
-			<div className="campfire-wrapper">
-				<div className="tree-container-back">
-					<div className="tree-8"></div>
-					<div className="tree-9"></div>
-					<div className="tree-10"></div>
-				</div>
-				<div className="rock-container">
-					<div className="rock-big"></div>
-					<div className="rock-small">
-						<div className="rock-1"></div>
-						<div className="rock-2"></div>
-						<div className="rock-3"></div>
-						<div className="rock-4"></div>
-					</div>
-				</div>
-				<div className="smoke-container">
-					<svg>
-					<path d="M 150 0 Q 200 100 100 250 C 0 450 120 400 50 600  " />
-				</svg>
-					<div className="fire-container">
+class Intro extends Component {
 
-						<div className="flame-1"></div>
-						<div className="flame-2"></div>
-						<div className="flame-3"></div>
+	render() {
+		return (
+			<div>
+			<div id="clouds">
+				<div className="cloud x1"></div>
+				<div className="cloud x2"></div>
+				<div className="cloud x3"></div>
+				<div className="cloud x4"></div>
+				<div className="cloud x5"></div>
+				<div className="cloud x6"></div>
+				<div className="cloud x7"></div>
+				<div className="cloud x8"></div>
+				<div className="cloud x9"></div>
+				<div className="cloud x10"></div>
+			</div>
+		
+			<div className="container">
+				<div className="campfire-wrapper">
+					<div className="tree-container-back">
+						<div className="tree-8"></div>
+						<div className="tree-9"></div>
+						<div className="tree-10"></div>
+					</div>
+					<div className="rock-container">
+						<div className="rock-big"></div>
+						<div className="rock-small">
+							<div className="rock-1"></div>
+							<div className="rock-2"></div>
+							<div className="rock-3"></div>
+							<div className="rock-4"></div>
+						</div>
+					</div>
+					<div className="smoke-container">
+						<svg>
+							<path d="M 150 0 Q 200 100 100 250 C 0 450 120 400 50 600  " />
+						</svg>
+						<div className="fire-container">
+
+							<div className="flame-1"></div>
+							<div className="flame-2"></div>
+							<div className="flame-3"></div>
+						</div>
+					</div>
+					<div className="tree-container-front">
+						<div className="tree-1"></div>
+						<div className="tree-2"></div>
+						<div className="tree-3"></div>
+						<div className="tree-4"></div>
+						<div className="tree-5"></div>
+						<div className="tree-6"></div>
+						<div className="tree-7"></div>
 					</div>
 				</div>
-				<div className="tree-container-front">
-					<div className="tree-1"></div>
-					<div className="tree-2"></div>
-					<div className="tree-3"></div>
-					<div className="tree-4"></div>
-					<div className="tree-5"></div>
-					<div className="tree-6"></div>
-					<div className="tree-7"></div>
 				</div>
 			</div>
-		</div>
-    )
+		)
+	}
 }
 
 export default Intro;
